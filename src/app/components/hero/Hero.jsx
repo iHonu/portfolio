7edc67a54a2a6a21a9c6aba01bc76bebbae1ae1a
Anastasia Tikhonova <honu347@gmail.com>
2024-04-19 16:02:28 +0200

    text format

--- src/app/components/hero/Hero.jsx
+++ src/app/components/hero/Hero.jsx
@@ -1,100 +1,175 @@
-'use client';
+"use client";
 import Link from "next/link";
 import Nav from "./Nav";
 import { motion, AnimatePresence, spring } from "framer-motion";
 import { useState } from "react";
 
-
-
-
-const heading1 = 'Creative';
-const heading2 = 'Web Developer';
-
+const heading1 = "Creative";
+const heading2 = "Web Developer";
 
 export default function Hero() {
+  const textRoundAnimation = {
+    hidden: { opacity: 0, scale: 0, x: 20, y: 70 },
+    enter: {
+      opacity: 1,
+      scale: 0.9,
+      x: 0,
+      y: 0,
+      rotate: -360,
+      transition: { type: "spring", stiffness: 80, delay: 0.5 },
+    },
+    hovered: {
+      rotate: 360,
+      transition: { duration: 8, ease: "linear", repeat: Infinity },
+    },
+  };
 
-   
+  const [isHovered, setIsHovered] = useState(false);
+  const textRound = isHovered ? {} : textRoundAnimation.hovered;
 
-    const textRoundAnimation = {
-        hidden: { opacity: 0, scale: 0, x: 20, y: 70},
-        enter: { opacity: 1, scale: .9, x:0, y:0, rotate:-360, transition: {type:'spring', stiffness: 80, delay: 0.5}},
-        hovered:  { rotate:360, transition: {duration:8, ease:"linear", repeat:Infinity}}, 
-    }
+  const headerAnimation = {
+    hidden: { opacity: 0, y: 60, x: 10 },
+    visible1: {
+      opacity: 1,
+      y: 0,
+      x: 0,
+      transition: { type: "spring", stiffness: 100 },
+    },
+    visible2: {
+      opacity: 1,
+      y: 0,
+      x: 0,
+      transition: { type: "spring", stiffness: 110, delay: 0.3 },
+    },
+    bg: {
+      backgroundColor: "#0d0101",
+      color: "#ffffff",
+      transition: { duration: 0.5, ease: [0.17, 0.67, 0.83, 0.67] },
+    },
+  };
 
-    const [isHovered, setIsHovered] = useState(false);
-    const textRound = isHovered ? { } : textRoundAnimation.hovered
+  const entryAnimation = {
+    hidden: { opacity: 0, y: 30 },
+    slideIn: {
+      opacity: 1,
+      y: 0,
+      transition: { type: "spring", stiffness: 120, delay: 0.5 },
+    },
+  };
 
-    const headerAnimation = {
-        hidden: { opacity: 0,  y: 60, x: 10},
-        visible1: { opacity: 1,  y: 0, x:0,  transition: {type:'spring', stiffness: 100}, },
-        visible2: { opacity: 1,  y: 0, x:0,  transition: {type:'spring', stiffness: 110, delay: 0.3}, },
-        bg: { backgroundColor: "#0d0101", color: "#ffffff", transition: {duration: 0.5,  ease: [0.17, 0.67, 0.83, 0.67]} }
-    }
+  return (
+    <div className="h-screen w-full flex flex-col px-16 py-8 z-10  max-w-[120rem] mx-auto">
+      <Nav />
+      <div className="relative flex-grow w-full flex flex-col items-center justify-center ">
+        {/* ADDITIONAL TEXT */}
+        <motion.div
+          variants={entryAnimation}
+          initial="hidden"
+          animate="slideIn"
+          className="absolute w-2/3 md:w-1/3 lg:w-1/5 bottom-[10vh] right-[10vw]"
+        >
+          <span className=" md:text-xl xl:tracking-wide xl:text-2xl text-zinc-500 ">
+            Creating web applications with attention to details and design
+          </span>
+        </motion.div>
 
-    const entryAnimation = {
-        hidden: { opacity: 0,  y: 30, },
-        slideIn:{ opacity: 1,  y: 0, transition: {type:'spring', stiffness: 120, delay: .5}, },
-    }
+        {/* TEXT CIRCLE */}
+        <div className="absolute top-[10vh] right-[5vw] xl:right-[12vw] xl:top-[2vh]">
+          <motion.div
+            onHoverStart={() => setIsHovered(true)}
+            onHoverEnd={() => setIsHovered(false)}
+            variants={textRoundAnimation}
+            initial="hidden"
+            animate="enter"
+            className="hidden md:inline-block cursor-default"
+          >
+            <motion.svg
+              animate={textRound}
+              whileHover={{
+                scale: 1.3,
+                transition: { ease: "easeOut", duration: 0.4 },
+              }}
+              viewBox="0 0 300 300"
+              className="w-64 h-64 xl:w-96 xl:h-96"
+            >
+              <defs>
+                <path
+                  id="circlePath"
+                  d="M 150, 150 m -60, 0 a 60,60 0 1,1 120,0 a 60,60 0 1,1 -120,0"
+                ></path>
+              </defs>
+              <text>
+                <textPath xlinkHref="#circlePath" className="text-lg">
+                  FULLSTACK DEVELOPER * FULLSTACK DEVELOPER *
+                </textPath>
+              </text>
+            </motion.svg>
 
-    return (
-        <div className="h-screen w-full flex flex-col px-16 py-8 z-10  max-w-[120rem] mx-auto">
-            <Nav />
-            <div className="relative flex-grow w-full flex flex-col items-center justify-center " >
-                
-                {/* ADDITIONAL TEXT */}
-                <motion.div variants={entryAnimation} initial='hidden' animate='slideIn' className="absolute w-2/3 md:w-1/3 lg:w-1/5 bottom-[10vh] right-[10vw]">
-                    <span className=" md:text-xl xl:tracking-wide xl:text-2xl text-zinc-500 ">Creating web applications with attention to details and design</span>
-                </motion.div>
-
-                {/* TEXT CIRCLE */}
-                <div className="absolute top-[10vh] right-[5vw] xl:right-[12vw] xl:top-[2vh]">
-                    <motion.div onHoverStart={() => setIsHovered(true)}
-                            onHoverEnd={() => setIsHovered(false)} variants={textRoundAnimation} initial='hidden' animate='enter' className="hidden md:inline-block cursor-default">
-                        <motion.svg animate={textRound}  whileHover={{ scale: 1.3, transition: {ease: "easeOut", duration: .4} }}
-                            viewBox="0 0 300 300" className="w-64 h-64 xl:w-96 xl:h-96">
-                            <defs>
-                                <path id="circlePath" d="M 150, 150 m -60, 0 a 60,60 0 1,1 120,0 a 60,60 0 1,1 -120,0"></path>
-                            </defs>
-                            <text>
-                                <textPath xlinkHref="#circlePath" className="text-lg">
-                                FULLSTACK DEVELOPER * FULLSTACK DEVELOPER *
-                                </textPath>
-                            </text>
-                        </motion.svg>
-
-                        <Link href="/" className=" w-16 h-16 xl:w-24 xl:h-24 absolute top-0 left-0 right-0 bottom-0 m-auto text-white bg-black rounded-full flex items-center justify-center 
-                         hover:scale-90 ease-out duration-100">
-                            Resume
-                        </Link>
-
-                    </motion.div>
-                </div>
-
-                {/* HERO H1 */}
-                <div className=" relative flex flex-col gap-6 items-center justify-start">
-
-                    {/* FIRST LINE */}
-                    <motion.div variants={headerAnimation} initial="hidden" animate="visible1" className=" flex w-full justify-start items-center gap-12 md:px-2">
-
-                        <motion.span  whileHover='bg' transition={{staggerChildren: 0.05}} className="text-5xl bg-white md:text-6xl lg:text-7xl xl:text-8xl font-light">{heading1.split('').map((char, i) => (<motion.span key={i} variants={headerAnimation}>{char}</motion.span>))}</motion.span>
-                     
-                    </motion.div>
-
-                    {/* SECOND LINE */}
-                    <motion.div variants={headerAnimation}  initial="hidden" animate="visible2" className=" flex w-full justify-start items-center gap-4 ">
-                     
-                        <motion.span whileHover='bg' transition={{staggerChildren: 0.05}} className="text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-bold">{heading2.split('').map((char, i) => (<motion.span key={i} variants={headerAnimation}>{char}</motion.span>))}</motion.span>
-
-                    </motion.div>
-                </div>
-            </div>
-            
-            {/* BOTTOM TEXT */}
-            <motion.div variants={entryAnimation} initial='hidden' animate='slideIn' className="relative w-1/2 font-mono text-xl h-12 overflow-hidden flex items-center"> 
-                <span className="font-mono text-base md:text-xl hover:text-red-600 hover:-translate-y-1 duration-300 ease-out  ">Anastasia Tikhonova</span>
-            </motion.div>
+            <Link
+              href="/"
+              className=" w-16 h-16 xl:w-24 xl:h-24 absolute top-0 left-0 right-0 bottom-0 m-auto text-white bg-black rounded-full flex items-center justify-center 
+                         hover:scale-90 ease-out duration-100"
+            >
+              Resume
+            </Link>
+          </motion.div>
         </div>
-    );
+
+        {/* HERO H1 */}
+        <div className=" relative flex flex-col gap-6 items-center justify-start">
+          {/* FIRST LINE */}
+          <motion.div
+            variants={headerAnimation}
+            initial="hidden"
+            animate="visible1"
+            className=" flex w-full justify-start items-center gap-12 md:px-2"
+          >
+            <motion.span
+              whileHover="bg"
+              transition={{ staggerChildren: 0.05 }}
+              className="text-5xl bg-white md:text-6xl lg:text-7xl xl:text-8xl font-light"
+            >
+              {heading1.split("").map((char, i) => (
+                <motion.span key={i} variants={headerAnimation}>
+                  {char}
+                </motion.span>
+              ))}
+            </motion.span>
+          </motion.div>
+
+          {/* SECOND LINE */}
+          <motion.div
+            variants={headerAnimation}
+            initial="hidden"
+            animate="visible2"
+            className=" flex w-full justify-start items-center gap-4 "
+          >
+            <motion.span
+              whileHover="bg"
+              transition={{ staggerChildren: 0.05 }}
+              className="text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-bold"
+            >
+              {heading2.split("").map((char, i) => (
+                <motion.span key={i} variants={headerAnimation}>
+                  {char}
+                </motion.span>
+              ))}
+            </motion.span>
+          </motion.div>
+        </div>
+      </div>
+
+      {/* BOTTOM TEXT */}
+      <motion.div
+        variants={entryAnimation}
+        initial="hidden"
+        animate="slideIn"
+        className="relative w-1/2 font-mono text-xl h-12 overflow-hidden flex items-center"
+      >
+        <span className="font-mono text-base md:text-xl hover:text-red-600 hover:-translate-y-1 duration-300 ease-out  ">
+          Anastasia Tikhonova
+        </span>
+      </motion.div>
+    </div>
+  );
 }
-
-
--- src/app/components/hero/Nav.jsx
+++ src/app/components/hero/Nav.jsx
@@ -1,78 +1,80 @@
-'use client';
+"use client";
 
 import Link from "next/link";
 import { motion } from "framer-motion";
 
 const links = [
-    { title: "Linkedin", url: "https://www.linkedin.com/in/honu347/" },
-    { title: "GitHub", url: "https://github.com/iHonu" },
-    { title: "Contact", url: "/contact" },
+  { title: "Linkedin", url: "https://www.linkedin.com/in/honu347/" },
+  { title: "GitHub", url: "https://github.com/iHonu" },
+  { title: "Contact", url: "/contact" },
 ];
 
 export default function Nav() {
-    const navVariants = {
-        hidden: { opacity: 0, y: -60, x: 10 },
-        visible: { 
-            opacity: 1, y: 0, x: 0,  
-            transition: { type: 'spring', stiffness: 100 },
-        },
-    };
+  const navVariants = {
+    hidden: { opacity: 0, y: -60, x: 10 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      x: 0,
+      transition: { type: "spring", stiffness: 100 },
+    },
+  };
 
-    const linksContainerVariants = {
-        hidden: { opacity: 0 },
-        visible: {
-            opacity: 1,
-            transition: { staggerChildren: 0.2 },
-        },
-    };
+  const linksContainerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: { staggerChildren: 0.2 },
+    },
+  };
 
-    const linkVariants = {
-        hidden: { opacity: 0, y: 20 },
-        visible: { 
-            opacity: 1, y: 0,
-            transition: { type: 'spring', stiffness: 100 },
-        },
-    };
+  const linkVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { type: "spring", stiffness: 100 },
+    },
+  };
 
-    return (
-        <div className="flex justify-between relative">
-            {/* INFO */}
-            <div className="w-2/3 md:w-1/3 flex flex-col overflow-hidden justify-between md:flex-row">
-                <motion.p
-                    variants={navVariants}
-                    initial="hidden"
-                    animate="visible"
-                    className="font-mono text-sm"
-                >
-                    honu347@gmail.com
-                </motion.p>
-                <motion.p
-                    variants={navVariants}
-                    initial="hidden"
-                    animate="visible"
-                    className="font-mono text-sm hidden md:inline-block"
-                >
-                    Based in the Netherlands
-                </motion.p>
-            </div>
-            {/* LINKS */}
-            <motion.div
-                className="relative flex items-end flex-col font-mono gap-2 tracking-wide duration-75"
-                variants={linksContainerVariants}
-                initial="hidden"
-                animate="visible"
-            >
-                {links.map((link) => (
-                    <motion.div
-                        key={link.title}
-                        variants={linkVariants}
-                        className="px-1  cursor-pointer overflow-hidden bg-blue-400"
-                    >
-                        <Link href={link.url}>{link.title}</Link>
-                        
-                    </motion.div>
-                ))}
-            </motion.div>
-        </div>
-    );
+  return (
+    <div className="flex justify-between relative">
+      {/* INFO */}
+      <div className="w-2/3 md:w-1/3 flex flex-col overflow-hidden justify-between md:flex-row">
+        <motion.p
+          variants={navVariants}
+          initial="hidden"
+          animate="visible"
+          className="font-mono text-sm"
+        >
+          honu347@gmail.com
+        </motion.p>
+        <motion.p
+          variants={navVariants}
+          initial="hidden"
+          animate="visible"
+          className="font-mono text-sm hidden md:inline-block"
+        >
+          Based in the Netherlands
+        </motion.p>
+      </div>
+      {/* LINKS */}
+      <motion.div
+        className="relative flex items-end flex-col font-mono gap-2 tracking-wide duration-75"
+        variants={linksContainerVariants}
+        initial="hidden"
+        animate="visible"
+      >
+        {links.map((link) => (
+          <motion.div
+            key={link.title}
+            variants={linkVariants}
+            className="px-1  cursor-pointer overflow-hidden bg-blue-400"
+          >
+            <Link href={link.url}>{link.title}</Link>
+          </motion.div>
+        ))}
+      </motion.div>
+    </div>
+  );
 }
